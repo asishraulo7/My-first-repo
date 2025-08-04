827cc87e91de25134cf13091a23a2250e1677998
asishraulo7 <78639802+asishraulo7@users.noreply.github.com>
2025-08-04 11:40:57 +0530

    Update spotify.java

--- spotify.java
+++ spotify.java
@@ -1,11 +1,6 @@
-public class EvenOdd {
+public class HelloWorld {
     public static void main(String[] args) {
-        int num = 7;
-
-        if (num % 2 == 0) {
-            System.out.println(num + " is even.");
-        } else {
-            System.out.println(num + " is odd.");
-        }
+        // This line prints "Hello, World!" to the console.
+        System.out.println("Hello, World!"); 
     }
 }
